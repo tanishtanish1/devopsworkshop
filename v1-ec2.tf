9508f3b435a9a8e5cd90cccb6682e7b7145e301f
tanishtanish1 <siddam.thaneesh@gmail.com>
2023-10-11 14:14:13 +1000

    ec2-instance

--- v1-ec2.tf
+++ v1-ec2.tf
@@ -2,22 +2,29 @@ provider "aws" {
   region = "us-east-2"
 }
 resource "aws_instance" "demo-server" {
-    ami = "ami-036f5574583e16426"
+    ami = "ami-024e6efaf93d85776"
     instance_type = "t2.micro"
     key_name = "dps"
-    security_groups = ["demo-sg"]
+    //security_groups = ["demo-sg"]
+    vpc_security_group_ids = [aws_security_group.demo-sg.id]
+    subnet_id = aws_subnet.dpp-public-subnet-01.id
+    for_each = toset(["Ansible","jenins-master","Build-slave"])
+    tags = {
+      Name ="${each.key}" 
+    }
   
 }
 
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
   description = "SSH-Access"
+  vpc_id = aws_vpc.dpp-vpc.id
   
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 20
-    to_port          = 20
+    description      = "ssh access"
+    from_port        = 22
+    to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     
@@ -32,6 +39,70 @@ resource "aws_security_group" "demo-sg" {
   }
 
   tags = {
-    Name = "ssh access"
+    Name = "ssh-port"
   }
 }
+
+resource "aws_vpc" "dpp-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name ="dpp-vpc"
+    
+  }
+  }
+
+resource "aws_subnet" "dpp-public-subnet-01" {
+  vpc_id            = aws_vpc.dpp-vpc.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-2a"
+  tags = {
+    Name = "dpp-Public-Subnet-01"
+  }
+  
+}
+
+resource "aws_subnet" "dpp-public-subnet-02" {
+  vpc_id            = aws_vpc.dpp-vpc.id
+  cidr_block = "10.1.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-2b"
+  tags = {
+    Name = "dpp-Public-Subnet-02"
+  }
+  
+}
+
+resource "aws_internet_gateway" "dpp-igw" {
+  vpc_id		  	  = aws_vpc.dpp-vpc.id
+  tags = {
+    Name = "dpp-igw"
+
+    }
+  }
+
+  resource "aws_route_table" "dpp-public-rt" {
+    vpc_id           = aws_vpc.dpp-vpc.id
+    route  {
+      cidr_block = "0.0.0.0/0"
+      gateway_id =aws_internet_gateway.dpp-igw.id
+    }
+
+    
+  }
+  resource "aws_route_table_association" "dpp-rta-public-subnet-01" {
+    subnet_id     		  =  aws_subnet.dpp-public-subnet-01.id
+    route_table_id 	   	   = aws_route_table.dpp-public-rt.id
+    
+  }
+
+
+resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
+    subnet_id     		  =  aws_subnet.dpp-public-subnet-02.id
+    route_table_id 	   	   = aws_route_table.dpp-public-rt.id
+    
+  }
+  
+
+
+
